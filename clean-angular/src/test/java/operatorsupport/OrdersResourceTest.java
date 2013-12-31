a69752587efbd1804bf17f3903efb7bd631cd109
michal.michaluk <michaluk.michal@gmail.com>
2013-12-31 01:14:41 +0100

    #10 OrdersResourceTest extended

--- clean-angular/src/test/java/operatorsupport/OrdersResourceTest.java
+++ clean-angular/src/test/java/operatorsupport/OrdersResourceTest.java
@@ -1,12 +1,9 @@
 package operatorsupport;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import static orderprocessing.OrderBuilder.anOrder;
-import orderprocessing.OrderEntity;
 import orderprocessing.OrderKey;
 import static orderprocessing.OrderSequenceBuilder.anOrderProcessingSequence;
 import orderprocessing.OrderState;
@@ -45,8 +42,10 @@ public class OrdersResourceTest {
     public void shouldReturnEmptySequence() {
         // given
         String operator = "michal";
+        OrderKey someKey = new OrderKey("123400001", "B1", "2013");
         OrderKey[] emptySequence = {};
         persistenceUnit.persist(
+                anOrder().likeSomeNew8ROrder().withOrderKey(someKey).withOrderState(OrderState.OPEN).build(),
                 anOrderProcessingSequence(operator).withOrders(emptySequence).build());
 
         // when
@@ -61,14 +60,17 @@ public class OrdersResourceTest {
     public void shouldReturnOrdersForMichalWhenOneSimpleSequenceExists() {
         // given
         String operator = "michal";
-        OrderKey[] expectedOrderProcessingSequence = {
-            new OrderKey("123456789", "A1", "2013"),
-            new OrderKey("987654321", "A2", "2013"),
-            new OrderKey("564738291", "A1", "2013")
-        };
+
+        OrderKey processedKey = new OrderKey("123456789", "A1", "2013");
+        OrderKey scheduledKey1 = new OrderKey("987654321", "A2", "2013");
+        OrderKey scheduledKey2 = new OrderKey("564738291", "A1", "2013");
+
+        OrderKey[] sequencedKeys = {processedKey, scheduledKey1, scheduledKey2};
         persistenceUnit.persist(
-                someScheduledOrdersWithKeys(expectedOrderProcessingSequence),
-                anOrderProcessingSequence(operator).withOrders(expectedOrderProcessingSequence).build()
+                anOrder().likeSomeNew8ROrder().withOrderKey(processedKey).withOrderState(OrderState.PROCESSED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(scheduledKey1).withOrderState(OrderState.SCHEDULED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(scheduledKey2).withOrderState(OrderState.SCHEDULED).build(),
+                anOrderProcessingSequence(operator).withOrders(sequencedKeys).build()
         );
 
         // when
@@ -78,12 +80,12 @@ public class OrdersResourceTest {
         JsonArrayBuilder expectedJsonObjects = Json.createArrayBuilder();
         expectedJsonObjects.add(Json.createObjectBuilder()
                 .add("key", "123456789/A1/2013")
-                .add("state", "todo")
+                .add("state", "done")
                 .add("products", Json.createArrayBuilder()
                         .add(Json.createObjectBuilder()
-                                .add("key", "ksr-14-378").add("done", 0).add("qty", 1))
+                                .add("key", "ksr-14-378").add("done", 1).add("qty", 1))
                         .add(Json.createObjectBuilder()
-                                .add("key", "jre-07-666").add("done", 0).add("qty", 2))
+                                .add("key", "jre-07-666").add("done", 2).add("qty", 2))
                 )
         ).add(Json.createObjectBuilder()
                 .add("key", "987654321/A2/2013")
@@ -110,13 +112,69 @@ public class OrdersResourceTest {
                 .isEqualTo(expected);
     }
 
-    private List<OrderEntity> someScheduledOrdersWithKeys(OrderKey[] expectedOrderProcessingSequence) {
-        List<OrderEntity> orders = new ArrayList<>(expectedOrderProcessingSequence.length);
-        for (OrderKey orderKey : expectedOrderProcessingSequence) {
-            OrderEntity order = anOrder().likeSomeNew8ROrder()
-                    .withOrderKey(orderKey).withOrderState(OrderState.SCHEDULED).build();
-            orders.add(order);
-        }
-        return orders;
+    @Test
+    public void shouldNotReturnOpenAndClosedOrders() {
+        // given
+        String operator = "michal";
+
+        OrderKey closedKey = new OrderKey("123400000", "B1", "2013");
+        OrderKey processedKey = new OrderKey("123456789", "A1", "2013");
+        OrderKey scheduledKey = new OrderKey("564738291", "A1", "2013");
+        OrderKey openKey = new OrderKey("123400001", "B1", "2013");
+
+        OrderKey[] sequencedKeys = {closedKey, processedKey, scheduledKey};
+
+        persistenceUnit.persist(
+                anOrder().likeSomeNew8ROrder().withOrderKey(closedKey).withOrderState(OrderState.CLOSED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(processedKey).withOrderState(OrderState.PROCESSED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(scheduledKey).withOrderState(OrderState.SCHEDULED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(openKey).withOrderState(OrderState.OPEN).build(),
+                anOrderProcessingSequence(operator).withOrders(sequencedKeys).build()
+        );
+
+        // when
+        String json = ordersResourceUnderTest.getOrders(operator);
+
+        // then
+        assertThat(json)
+                .doesNotContain(openKey.toString())
+                .doesNotContain(closedKey.toString())
+                .containsOnlyOnce(processedKey.toString())
+                .containsOnlyOnce(scheduledKey.toString());
     }
+
+    @Test
+    public void shouldNotReturnOrdersAssignedToOtherOperator() {
+        // given
+        String michal = "michal";
+        String krzysztof = "krzysztof";
+
+        OrderKey michalKey1 = new OrderKey("123456789", "A1", "2013");
+        OrderKey krzysztofKey1 = new OrderKey("564738291", "A1", "2013");
+        OrderKey krzysztofKey2 = new OrderKey("123400000", "B1", "2013");
+        OrderKey michalKey2 = new OrderKey("123400001", "B1", "2013");
+
+        OrderKey[] sequencedKeysAssignedToMichal = {michalKey1, michalKey2};
+        OrderKey[] sequencedKeysAssignedToKrzysztof = {krzysztofKey1, krzysztofKey2};
+
+        persistenceUnit.persist(
+                anOrder().likeSomeNew8ROrder().withOrderKey(michalKey1).withOrderState(OrderState.PROCESSED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(krzysztofKey1).withOrderState(OrderState.PROCESSED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(krzysztofKey2).withOrderState(OrderState.SCHEDULED).build(),
+                anOrder().likeSomeNew8ROrder().withOrderKey(michalKey2).withOrderState(OrderState.SCHEDULED).build(),
+                anOrderProcessingSequence(michal).withOrders(sequencedKeysAssignedToMichal).build(),
+                anOrderProcessingSequence(krzysztof).withOrders(sequencedKeysAssignedToKrzysztof).build()
+        );
+
+        // when
+        String json = ordersResourceUnderTest.getOrders(michal);
+
+        // then
+        assertThat(json)
+                .doesNotContain(krzysztofKey1.toString())
+                .doesNotContain(krzysztofKey2.toString())
+                .containsOnlyOnce(michalKey1.toString())
+                .containsOnlyOnce(michalKey2.toString());
+    }
+
 }
